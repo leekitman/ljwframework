f5cd090bb39315fa94bf0980fa048b40045dacad
lijiewenn <lijiewen2015@163.com>
2018-04-30 00:56:00 +0800

    进行到：第二章完结

--- ljwframework/src/main/java/com/leekitman/practise/ljwframework/controller/CustomerServlet.java
+++ ljwframework/src/main/java/com/leekitman/practise/ljwframework/controller/CustomerServlet.java
@@ -1,17 +1,29 @@
 package com.leekitman.practise.ljwframework.controller;
 
+import com.leekitman.practise.ljwframework.model.Customer;
+import com.leekitman.practise.ljwframework.service.CustomerService;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 /**
  * 创建客户
  */
-@WebServlet("/customer_create")
-public class CustomerCreateServlet extends HttpServlet {
+@WebServlet("/customer")
+public class CustomerServlet extends HttpServlet {
+
+    private CustomerService customerService;
+
+    @Override
+    public void init(){
+        customerService = new CustomerService();
+    }
+
     /**
      * 进入创建客户界面
      * @param req
@@ -21,7 +33,9 @@ public class CustomerCreateServlet extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        // TODO
+        List<Customer> customerList = customerService.getCustomerList();
+        req.setAttribute("customerList",customerList);
+        req.getRequestDispatcher("/WEB-INF/view/customer.jsp").forward(req,resp);
     }
 
     /**
