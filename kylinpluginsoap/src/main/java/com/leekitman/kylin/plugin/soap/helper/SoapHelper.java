c6f7dff39bf3e0ff5c8f289d57077041c08bf6c4
lijiewenn <lijiewen2015@163.com>
2018-05-11 17:36:02 +0800

    SOAP 服务插件完结

--- kylinpluginsoap/src/main/java/com/leekitman/kylin/plugin/soap/helper/SoapHelper.java
+++ kylinpluginsoap/src/main/java/com/leekitman/kylin/plugin/soap/helper/SoapHelper.java
@@ -1,8 +1,11 @@
 package com.leekitman.kylin.plugin.soap.helper;
 
 import com.leekitman.kylin.plugin.soap.SoapConfig;
+import org.apache.cxf.frontend.ClientProxyFactoryBean;
+import org.apache.cxf.frontend.ServerFactoryBean;
 import org.apache.cxf.interceptor.Interceptor;
 import org.apache.cxf.interceptor.LoggingInInterceptor;
+import org.apache.cxf.interceptor.LoggingOutInterceptor;
 import org.apache.cxf.message.Message;
 
 import java.util.ArrayList;
@@ -21,8 +24,36 @@ public class SoapHelper {
 
     static {
         // 添加 Logging Interceptor
-        if(SoapConfig.isLog()){
-            new LoggingInInterceptor();
+        if (SoapConfig.isLog()) {
+            LoggingInInterceptor loggingInInterceptor = new LoggingInInterceptor();
+            inInterceptorList.add(loggingInInterceptor);
+            LoggingOutInterceptor loggingOutInterceptor = new LoggingOutInterceptor();
+            outInterceptorList.add(loggingOutInterceptor);
         }
+    }
+
+    /**
+     * 发布 SOAP 服务
+     */
+    public static void publishService(String wsdl, Class<?> interfaceClass, Object implementInstance) {
+        ServerFactoryBean factory = new ServerFactoryBean();
+        factory.setAddress(wsdl);
+        factory.setServiceClass(interfaceClass);
+        factory.setServiceBean(implementInstance);
+        factory.setInInterceptors(inInterceptorList);
+        factory.setOutInterceptors(outInterceptorList);
+        factory.create();
+    }
+
+    /**
+     * 创建 SOAP 客户端
+     */
+    public static <T> T createClient(String wsdl, Class<? extends T> interfaceClass) {
+        ClientProxyFactoryBean factory = new ClientProxyFactoryBean();
+        factory.setAddress(wsdl);
+        factory.setServiceClass(interfaceClass);
+        factory.setInInterceptors(inInterceptorList);
+        factory.setOutInterceptors(outInterceptorList);
+        return factory.create(interfaceClass);
     }
 }
